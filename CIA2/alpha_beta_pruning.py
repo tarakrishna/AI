0014704ef540934ebe2c0774422bc4ffd116c82c
tarakrishna <131344390+tarakrishna@users.noreply.github.com>
2024-10-17 23:46:15 +0530

    Update alpha_beta_pruning.py

--- CIA2/alpha_beta_pruning.py
+++ CIA2/alpha_beta_pruning.py
@@ -1,35 +1,30 @@
-def minimax(depth, nodeIndex, isMaximizingPlayer, values, alpha, beta):
-
-    if depth == 3:
-        return values[nodeIndex]
+def alpha_beta(depth, node_index, is_maximizing, leaf_values, alpha, beta):
+    if depth == 2:
+        return leaf_values[node_index]
     
-    if isMaximizingPlayer:
-        maxEval = float('-inf')
+    if is_maximizing:
+        max_score = float('-inf')
         for i in range(2):  
-            eval = minimax(depth + 1, nodeIndex * 2 + i, False, values, alpha, beta)
-            maxEval = max(maxEval, eval)
-            alpha = max(alpha, eval)
+            score = alpha_beta(depth + 1, node_index * 2 + i, False, leaf_values, alpha, beta)
+            max_score = max(max_score, score)
+            alpha = max(alpha, score)
             if beta <= alpha:
                 break  
-        return maxEval
+        return max_score
     else:
-        minEval = float('inf')
-        for i in range(2):  
-            eval = minimax(depth + 1, nodeIndex * 2 + i, True, values, alpha, beta)
-            minEval = min(minEval, eval)
-            beta = min(beta, eval)
+        min_score = float('inf')
+        for i in range(2): 
+            score = alpha_beta(depth + 1, node_index * 2 + i, True, leaf_values, alpha, beta)
+            min_score = min(min_score, score)
+            beta = min(beta, score)
             if beta <= alpha:
-                break 
-        return minEval
+                break  
+        return min_score
 
-# Driver code
 if __name__ == "__main__":
-    values = [1, 4, 7, 2, 3, 0, 6, 5]
-    
+    leaf_values = [4, 8, 9, 3, 2, -2, 9, 1]
 
     alpha = float('-inf')
     beta = float('inf')
-    
-    optimalValue = minimax(0, 0, True, values, alpha, beta)
-    
-    print(f"The optimal after the pruning process is {optimalValue}")
+    optimal_value = alpha_beta(0, 0, True, leaf_values, alpha, beta)
+    print(f"The optimal value after the pruning process is {optimal_value}")
